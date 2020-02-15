5303d1368aff8bdab3a0aa841df661a86e40a590
Floyd Tiu <floydrichard.tiu.iics@ust.edu.ph>
2020-02-16 00:56:13 +0800

    First commit

---
+++ billing.js
@@ -0,0 +1,17 @@
+/*Add the JavaScript here for the function billingFunction(). 
+ It is responsible for setting and clearing the fields in Billing Information */
+
+ let billingFunction = () =>
+ {  
+     var same = document.getElementById('same');
+     if(same.checked)
+     {
+         document.getElementById('billingName').value = document.getElementById('shippingName').value;
+         document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
+     }
+     else
+     {
+         document.getElementById('billingName') = " ";
+         document.getElementById('billingZip') = " ";
+     }
+ }
